18a428a06edb7a73a90a390e46f88923cacbda40
ADMIN <nikita.art.tar@gmail.com>
2026-01-07 08:32:40 +0530

    Revert "Changes"
    This reverts commit bcd15e3ff9ebee6d1810802c3aed1ca85420e914.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,7 @@
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
-    private static final Logger logger = LoggerFactory.getLogger(Hippodrome.class);
 
     public static void main(String[] args) throws Exception {
         List<Horse> horses = List.of(
@@ -18,14 +14,12 @@ public class Main {
                 new Horse("Cherry", 3)
         );
         Hippodrome hippodrome = new Hippodrome(horses);
-        logger.info("Начало скачек. Количество участников: {}", horses.size());
+
         for (int i = 0; i < 100; i++) {
             hippodrome.move();
             watch(hippodrome);
             TimeUnit.MILLISECONDS.sleep(200);
         }
-        logger.info("Окончание скачек. Победитель: {}", hippodrome.getWinner().getName());
-
 
         String winnerName = hippodrome.getWinner().getName();
         System.out.println(winnerName + " wins!");
